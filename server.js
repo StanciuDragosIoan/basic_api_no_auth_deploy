63e2c1bc730f01f002fbdb3e5d7dde5b15bff6dc
stanciudragosioan <stanciudragosioan92@gmail.com>
2021-04-14 00:12:01 +0300

    test2

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const http = require("http");
 const resource = require("./resource");
 
 //define port for our server to listen to
-const PORT = process.env.port || 3000;
+const PORT = process.env.port || 5000;
 
 //create server
 const server = http.createServer((req, res) => {
